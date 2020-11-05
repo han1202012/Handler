e6e49504fe2226d35753afea1db5a61d7e15f5de
han1202012 <octopus_truth@163.com>
2020-11-05 15:28:24 +0800

    update

--- app/src/main/java/kim/hsl/handler/MainActivity.kt
+++ app/src/main/java/kim/hsl/handler/MainActivity.kt
@@ -2,11 +2,17 @@ package kim.hsl.handler
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.MessageQueue
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
